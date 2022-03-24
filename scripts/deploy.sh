f10d373bc8ea01f19433b6c444aff5f02d9d939e
qudalsrnt3x <qudalsrnt3x@gmail.com>
2022-03-24 23:04:14 +0900

    자동 배포 환경 구성

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -34,9 +34,7 @@ chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
 
 nohup java -jar \
-  -Dspring.config.location=/home/ec2-user/app/application.yml, \
-                            /home/ec2-user/app/application-prod.yml, \
-                            /home/ec2-user/app/application-aws.yml \
+  -Dspring.config.location=/home/ec2-user/app/application.yml,/home/ec2-user/app/application-prod.yml,/home/ec2-user/app/application-aws.yml \
   -Dspring.profiles.active=prod \
   -Duser.timezone=Asia/Seoul
   $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
