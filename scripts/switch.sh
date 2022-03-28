12a52c9b24e0ffe9b92f5a3bd4cfa485782e9f55
qudalsrnt3x <qudalsrnt3x@gmail.com>
2022-03-29 00:19:52 +0900

    배포 스크립트 수정

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -11,6 +11,20 @@ function switch_proxy() {
     echo "> Port 전환"
     echo "set \$service_url https://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
 
+    # 포트 전환하면서, 바꾼거말고 다른거 kill
+    if [ ${IDLE_PORT} == 8444 ]
+    then
+      KILL_PORT=8445
+      IDLE_PID=$(lsof -ti tcp:${KILL_PORT})
+      echo "> ${KILL_PORT} 포트를 종료합니다."
+      sudo kill -15 ${IDLE_PID}
+    else
+      KILL_PORT=8444
+      IDLE_PID=$(lsof -ti tcp:${KILL_PORT})
+      echo "> ${KILL_PORT} 포트를 종료합니다."
+      sudo kill -15 ${IDLE_PID}
+    fi
+
     echo "> 엔진엑스 Reload"
     sudo systemctl reload nginx.service
 }
